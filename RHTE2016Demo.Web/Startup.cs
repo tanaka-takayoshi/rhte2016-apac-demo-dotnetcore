efcb411c736dd7ebb17c600240e00f2b5ffe6983
tanaka_733 <tanaka-takayoshi@users.noreply.github.com>
2016-09-02 15:58:59 +0900

    Added Secret.

--- RHTE2016Demo.Web/Startup.cs
+++ RHTE2016Demo.Web/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -12,6 +13,24 @@ namespace RHTE2016Demo.Web
 {
     public class Startup
     {
+        public IConfigurationRoot Configuration { get; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            builder.AddJsonFile("/config/secret.json", true);
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+
+            Console.WriteLine(Configuration["SecretKey"]);
+            Console.WriteLine(Configuration["SecretDict"]);
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
